fc5eb36629d748bbadce3adaacbe5697853dd271
Vinaychary Nangunoori <123128034+VINAYCHARY07@users.noreply.github.com>
2024-03-24 21:29:55 -0500

    updated few changes in certificate assistance webpage.
    Updated a few changes in the certificate assistance webpage.

---
+++ CertificateAssistanceForm.java
@@ -0,0 +1,11 @@
+package com.example.SMS.dto;
+
+import lombok.Data;
+
+@Data
+public class CertificateAssistanceForm {
+
+    private String requestType;
+    private String description;
+    private String emailId;
+}
