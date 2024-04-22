5bde7a8d4464c004cc06b413dcd95ac81a1fac47
Vinaychary Nangunoori <123128034+VINAYCHARY07@users.noreply.github.com>
2024-04-21 23:52:28 -0500

    all updated files of certificate assistance

--- CertificateAssistanceForm.java
+++ CertificateAssistanceForm.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class CertificateAssistanceForm {
 
+	private Long id;
     private String requestType;
     private String description;
     private String emailId;
--- EmailServiceImpl.java
+++ EmailServiceImpl.java
@@ -7,10 +7,14 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.SMS.dto.CertificateAssistanceForm;
 
 @Service
 @Transactional
@@ -35,27 +39,35 @@ public class EmailServiceImpl implements EmailService {
 	     javaMailSender.send(msg);
    }
 	
-	public void sendCertificateMessage(String to, String type) throws MessagingException {
-		
-		 MimeMessage msg = javaMailSender.createMimeMessage();
-	     MimeMessageHelper helper = new MimeMessageHelper(msg, true);
-	     helper.setTo(to);
-	     
-	     LocalDate currentDate = LocalDate.now();
-	        
-	        // Define a format
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        String formattedDate = currentDate.format(formatter);
-	        
-	     String subject = "New Certification Assistance is added!";
-	     String content = "<p>New Certification Assistance request for type "
-	                + "<b>" + type + "</b>"
-	                + " was added successfully on " 
-	                + formattedDate + "</p>";
-	         
-	     helper.setSubject(subject); 
-	     helper.setText(content, true);
-	     javaMailSender.send(msg);
-  }
+	public void sendCertificateMessage(CertificateAssistanceForm certificateAssistanceForm, MultipartFile file) throws Exception {
+
+	    MimeMessage msg = javaMailSender.createMimeMessage();
+	    MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+	    helper.setTo(certificateAssistanceForm.getEmailId());
+
+	    LocalDate currentDate = LocalDate.now();
+	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+	    String formattedDate = currentDate.format(formatter);
+
+	    String subject = "New Certification Assistance is added!";
+	    String content = "<html><body>"
+	            + "<p>New Certification Assistance request for type "
+	            + "<b>" + certificateAssistanceForm.getRequestType() + "</b>"
+	            + " was added successfully on " 
+	            + formattedDate + "</p>"
+	            + "<p> Description: "+ certificateAssistanceForm.getDescription() + "</p>"
+	            + "</body></html>";
+
+	    helper.setSubject(subject); 
+	    helper.setText(content, true);
+
+	    // Attach the file
+	    if (file != null && !file.isEmpty()) {
+	        ByteArrayResource fileResource = new ByteArrayResource(file.getBytes());
+	        helper.addAttachment(file.getOriginalFilename(), fileResource);
+	    }
+
+	    javaMailSender.send(msg);
+	}
 	
 }
